e3d0d3871c7fd1b972b4918d51fe7a1bd29f8bed
Dmitriy Maximov <d.maksimov@sftpro.ru>
2025-04-21 14:18:25 +0300

    fix rubocop

--- hexlet_code.gemspec
+++ hexlet_code.gemspec
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
 
-require_relative "lib/hexlet_code/version"
+require_relative 'lib/hexlet_code/version'
 
 Gem::Specification.new do |spec|
-  spec.name = "hexlet_code"
+  spec.name = 'hexlet_code'
   spec.version = HexletCode::VERSION
-  spec.authors = ["Dmitriy Maximov"]
-  spec.email = ["d.maksimov@sftpro.ru"]
+  spec.authors = ['Dmitriy Maximov']
+  spec.email = ['d.maksimov@sftpro.ru']
 
-  spec.summary = "Hexlet generator for forms"
-  spec.description = "Generator for forms in Hexlet style. It allows you to create forms with ease and flexibility, using a simple and intuitive syntax."
-  spec.homepage = "https://github.com/So1que/rails-project-63"
-  spec.license = "MIT"
-  spec.required_ruby_version = ">= 2.6.0"
+  spec.summary = 'Hexlet generator for forms'
+  spec.description = 'Generator for forms in Hexlet style. It allows you to create forms with ease and flexibility, ' \
+                     'using a simple and intuitive syntax.'
+  spec.homepage = 'https://github.com/So1que/rails-project-63'
+  spec.license = 'MIT'
+  spec.required_ruby_version = '>= 2.6.0'
 
-  spec.metadata["allowed_push_host"] = "https://example.com"
+  spec.metadata['allowed_push_host'] = 'https://example.com'
 
-  spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "https://github.com/So1que/rails-project-63"
-  spec.metadata["changelog_uri"] = "https://github.com/So1que/rails-project-63"
+  spec.metadata['homepage_uri'] = spec.homepage
+  spec.metadata['source_code_uri'] = 'https://github.com/So1que/rails-project-63'
+  spec.metadata['changelog_uri'] = 'https://github.com/So1que/rails-project-63'
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -27,9 +28,9 @@ Gem::Specification.new do |spec|
       (File.expand_path(f) == __FILE__) || f.start_with?(*%w[bin/ test/ spec/ features/ .git .circleci appveyor])
     end
   end
-  spec.bindir = "exe"
+  spec.bindir = 'exe'
   spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
   # Uncomment to register a new dependency of your gem
   # spec.add_dependency "example-gem", "~> 1.0"
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -33,9 +33,9 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_form_with_input_fields
-    expected = "<form action='#' method='post'><label for='name'>Name</label>"\
-    "<input name='name' type='text' value='rob'><label for='job'>Job</label>"\
-    "<input name='job' type='text' value='hexlet'></form>"
+    expected = "<form action='#' method='post'><label for='name'>Name</label>" \
+               "<input name='name' type='text' value='rob'><label for='job'>Job</label>" \
+               "<input name='job' type='text' value='hexlet'></form>"
     actual = HexletCode.form_for @user do |f|
       f.input :name
       f.input :job
@@ -44,8 +44,8 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_form_with_textarea
-    expected = "<form action='#' method='post'><label for='job'>Job</label>"\
-    "<textarea name='job' cols='20' rows='40'>hexlet</textarea></form>"
+    expected = "<form action='#' method='post'><label for='job'>Job</label>" \
+               "<textarea name='job' cols='20' rows='40'>hexlet</textarea></form>"
     actual = HexletCode.form_for @user do |f|
       f.input :job, as: :text
     end
@@ -53,8 +53,8 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_form_with_custom_attributes
-    expected = "<form action='#' method='post'><label for='name'>Name</label>"\
-    "<input name='name' type='text' value='rob' class='user-input'></form>"
+    expected = "<form action='#' method='post'><label for='name'>Name</label>" \
+               "<input name='name' type='text' value='rob' class='user-input'></form>"
     actual = HexletCode.form_for @user do |f|
       f.input :name, class: 'user-input'
     end
@@ -62,8 +62,8 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_form_with_textarea_custom_attrs
-    expected = "<form action='#' method='post'><label for='job'>Job</label>"\
-    "<textarea name='job' cols='50' rows='50'>hexlet</textarea></form>"
+    expected = "<form action='#' method='post'><label for='job'>Job</label>" \
+               "<textarea name='job' cols='50' rows='50'>hexlet</textarea></form>"
     actual = HexletCode.form_for @user do |f|
       f.input :job, as: :text, rows: 50, cols: 50
     end
@@ -79,8 +79,8 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_form_with_labels
-    expected = "<form action='#' method='post'><label for='name'>Name</label>"\
-    "<input name='name' type='text' value='rob'></form>"
+    expected = "<form action='#' method='post'><label for='name'>Name</label>" \
+               "<input name='name' type='text' value='rob'></form>"
     actual = HexletCode.form_for @user do |f|
       f.input :name
     end
@@ -102,9 +102,9 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_complete_form
-    expected = "<form action='#' method='post'><label for='name'>Name</label>"\
-    "<input name='name' type='text' value='rob'><label for='job'>Job</label>"\
-    "<input name='job' type='text' value='hexlet'><input type='submit' value='Save'></form>"
+    expected = "<form action='#' method='post'><label for='name'>Name</label>" \
+               "<input name='name' type='text' value='rob'><label for='job'>Job</label>" \
+               "<input name='job' type='text' value='hexlet'><input type='submit' value='Save'></form>"
     actual = HexletCode.form_for @user do |f|
       f.input :name
       f.input :job
